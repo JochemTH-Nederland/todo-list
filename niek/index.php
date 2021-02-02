fa870dde1f1ec467ea93e8267160af89ac43cdba
Niek Poorthuis <niek@appcenternederland.nl>
2021-02-02 10:26:24 +0100

    added php

---
+++ niek/index.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Retrieve the current stored todo list
+ */
+$todoList = json_decode(file_get_contents("todolist.json"), true);
+
+$newTodoList = [];
+
+
+switch ($_SERVER['REQUEST_METHOD']){
+    case "POST": //Add or update a list item
+        $requestData = $_POST;
+        break;
+    case "GET": //Retrieve all or a single list item
+        $requestData = $_GET;
+        break;
+}
+
+//Code here
+
+/**
+ * Store the new todo list
+ */
+file_put_contents("todolist.json", json_encode($newTodoList));
